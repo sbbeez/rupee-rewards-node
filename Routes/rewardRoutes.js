24221d5e65eec2f6b8107070e5b755c2671d8c88
senthilbalajiganesan <g.senthilbalaji@gmail.com>
2017-12-12 00:44:48 +0530

    Solved passport issue

--- Routes/rewardRoutes.js
+++ Routes/rewardRoutes.js
@@ -3,10 +3,9 @@ const passportServices = require("../services/passport");
 const passport = require("passport");
 const RewardController = require("../controllers/RewardController");
 
-const checkToken = passport.authenticate("jwt", { session: false });
+const checkToken = passport.authenticate("userLogin", { session: false });
 
 module.exports = app => {
-  app.get("/simple", checkToken, (req, res) => res.send("simplr"));
   app.get("/getRewards", checkToken, RewardController.getRewards);
   app.post("/addRewards", checkToken, RewardController.addRewards);
 };
--- services/passport.js
+++ services/passport.js
@@ -22,4 +22,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
   });
 });
 
-passport.use(jwtLogin);
+passport.use("userLogin", jwtLogin);
